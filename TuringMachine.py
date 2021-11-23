62ca64d5631f81f00ff7add2cdd7f871e963d8bb
Tony Hayden <haydena23@up.edu>
2021-11-22 16:12:49 -0800

    Rename

---
+++ TuringMachine.py
@@ -0,0 +1,69 @@
+# Using Python 3.9
+#
+# Authors:
+#   Tony Hayden
+#   Jennifer Brana
+# Version: 1.0
+# 11/22/21
+#
+# Notes:
+#   This program requires the termcolor module. To import this module, go to the terminal of this file
+#   and type "python -m pip install termcolor" without the "", and then restart. That should install the
+#   module. This module is being used to underline the head pointer on the tape.
+#
+# Resources used:
+#   Introducing Python: Modern Computing in Simple Packages, 2nd Edition (Bill Lubanovic)
+
+import itertools
+import collections
+
+class TuringMachine:
+    # Default constructor to create initial state
+    def __init__(
+        self,
+        startTape=(),
+        *,
+        blank='[]',
+        startState='q_s',
+        marker=0,):
+        # Sets self vars in accordance to initial vars
+        self.blank, self.state, self.marker = blank, startState, marker
+        # Creates tape by inserting blank space at the beginning, then the input string
+        self.tape = collections.defaultdict(lambda: self.blank, enumerate(startTape)) 
+    # Function to return the current char at head_position
+    @property
+    def char(self):
+        return self.tape[self.marker]
+    # Function to set the new char to current head position
+    @char.setter
+    def char(self, newChar):
+        self.tape[self.marker] = newChar
+    # Function to create the configuration in the form of leftHead::Head::rightHead
+    @property
+    def config(self):
+        indices = self.tape.keys() # Split tape into array using keys in format [0,1,2..tapeMax]
+        left, right = (min(indices), max(indices)) if indices else (0, 0) # Set the lowest and highest index of input string
+        leftHead, rightHead = (
+            tuple(self.tape[index] for index in range(*r)) for r in ((left, self.marker), (self.marker, max(right, self.marker)+1))
+        )
+        return leftHead, self.state, rightHead
+    # Function to remove blanks from string, and create tape by concatenating all chars on self.tape 
+    @property
+    def contents(self):
+        idx = self.tape.keys() # Parse the index values from the tape
+        leftBound, rightBound = (min(idx), max(idx)) if idx else (0,0) # Calculate bounds of the tape
+        # Function to remove any blank chars 
+        def remove_nulls(listToIterate):
+            return itertools.takewhile(
+                lambda char: char != self.blank, # If char == blank char, break loop and return iterable without blank chars
+                itertools.dropwhile(lambda char: char == self.blank, listToIterate) # If char == blank char, drop it and return
+            )
+        # Concatenate the tape iterable that now doesn't include blank chars
+        return ''.join(remove_nulls(self.tape[idx] for idx in range(leftBound,rightBound+1)))
+    def next(self, transition):
+        try:
+            NEWchar, NEWstate, markerMove = transition[self.state,self.char]
+        except KeyError:
+            raise RuntimeError(f'That transition is not defined')
+        self.char, self.state = NEWchar, NEWstate
+        self.marker = markerMove(self.marker)
